7f38e58c2a08d869b6be147074cc7e653943437e
ramya6599 <ramya6599@gmail.com>
2026-08-13 15:25:25 +0530

    Added Express server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log("my app is running at http://localhost:" + port);
+});
