7aaf6938b46e7203fa9258be389498dca9791b76
ramya6599 <ramya6599@gmail.com>
2026-08-14 14:14:39 +0530

    Upgrade GitPulse page

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,41 @@
-alert("Hello From Java Script");
+const users = [
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "image": "john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "image": "jane.png"
+    }
+];
+
+var curIndex = 0;
+
+function toggle() {
+    if (curIndex == 0)
+        curIndex = 1;
+    else
+        curIndex = 0;
+
+    document.getElementById("card-image").src = users[curIndex].image;
+    document.getElementById("card-name").innerText = users[curIndex].name;
+    document.getElementById("card-gender").innerText = users[curIndex].gender;
+}
+
+function random() {
+    fetch('https://randomuser.me/api')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var detail = data.results[0];
+
+            document.getElementById("card-image").src = detail.picture.large;
+            document.getElementById("card-gender").innerText = detail.gender;
+
+            var fullname = detail.name.first + " " + detail.name.last;
+            document.getElementById("card-name").innerText = fullname;
+        });
+}
